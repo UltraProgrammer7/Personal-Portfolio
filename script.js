ece392a46cb987196faf4efe0335dd3c420ecd62
UltraProgrammer7 <ultraprogrammer05@gmail.com>
2025-05-03 22:47:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Computer Programmer","Graphic Designer","Video Editor"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop: true
+})
